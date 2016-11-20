56740278545e52c37c7a8552865a9b2aa55d1386
devit47 <david.griffin0@gmail.com>
2016-11-20 22:38:33 +0000

    Exercise 1 complete

--- src/LabPractical7/Exercise1.java
+++ src/LabPractical7/Exercise1.java
@@ -1,8 +1,37 @@
 package LabPractical7;
 
+import java.util.Scanner;
+
 /**
  * Created by devit47 on 20/11/2016.
  */
 public class Exercise1 {
+    public static void main(String[] args) {
+        float average = 0, weight = 0, percentage;
+        int count = 0, numPeopleOver80kg = 0;
 
+        Scanner input = new Scanner(System.in);
+
+        while(count < 5){
+            System.out.print("Please enter the weight of person " + (count + 1) + ": ");
+            weight = input.nextFloat();
+
+            average += weight;
+
+            if(weight > 80.0f){
+                numPeopleOver80kg++;
+            }
+
+            count++;
+        }
+
+        average = average/5.0f;
+        percentage = (100 / count) * numPeopleOver80kg;
+
+        System.out.println("\n\n\n======================================" +
+                "\t\tProgram Results" +
+                "\n======================================" +
+                "\nAverage Weight: " + String.format("%.2f", average) + "kg" +
+                "\nPercentage >= 80kg: " + String.format("%.2f", percentage) + "%");
+    }
 }
